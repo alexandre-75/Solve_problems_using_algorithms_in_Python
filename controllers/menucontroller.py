48f229fb51ac2d2db536bb4df68dc01e13d14178
sassy alexandre <sassy.alexandre@gmail.com>
2023-03-07 11:13:21 +0100

    creation quit the program

--- controllers/menucontroller.py
+++ controllers/menucontroller.py
@@ -2,10 +2,17 @@ from views.menuview import MenuView
 
 
 class MenuController():
+
     def __init__(self):
         self.menu_view = MenuView()
-    
+
     def main_menu(self):
-        # while True:
         self.menu_view.display_main_menu()
-        print("end")
+        user_input = input("enter a value, please : ")
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            return exit()
+        else:
+            self.menu_view.error_choice_option()
+            return self.main_menu()
--- views/menuview.py
+++ views/menuview.py
@@ -1,5 +1,19 @@
 class MenuView():
+    
     def __init__(self):
         pass
+
     def display_main_menu(self):
-        print("test")
+        """
+        Prints the options available in the main menu.
+        """
+        print("Welcome to the main menu!")
+        print("What would you like to do? Select the desired option by entering its number:")
+        print("""
+        1. Analyze file 1: data (20 actions)
+        2. Quit the program
+        """)
+    
+    def error_choice_option(self):
+        """Prints an error message when the user's input is not understood."""
+        print("\nI did not understand your request, try again\n\n")
