ada665608f70d711c281a7e1bbee082dcc4618f8
sassy alexandre <sassy.alexandre@gmail.com>
2023-03-07 11:59:59 +0100

    create secondary menu

--- controllers/menucontroller.py
+++ controllers/menucontroller.py
@@ -10,9 +10,24 @@ class MenuController():
         self.menu_view.display_main_menu()
         user_input = input("enter a value, please : ")
         if user_input == "1":
-            pass
+            return self.secondary_menu_choice_of_program()
+            
         elif user_input == "2":
             return exit()
         else:
             self.menu_view.error_choice_option()
             return self.main_menu()
+    
+    def secondary_menu_choice_of_program (self):
+        self.menu_view.display_secondary_menu()
+        user_input = input("enter a value, please : ")
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            pass
+        elif user_input =="3":
+            return self.main_menu()
+        else:
+            self.menu_view.error_choice_option()
+            return self.secondary_menu_choice_of_program()
+        
--- views/menuview.py
+++ views/menuview.py
@@ -1,5 +1,5 @@
 class MenuView():
-    
+
     def __init__(self):
         pass
 
@@ -14,6 +14,17 @@ class MenuView():
         2. Quit the program
         """)
     
+    def display_secondary_menu(self):
+        """
+        Prints the options available in the secondary menu.
+        """
+        print("\nWhat would you like to do? Select the desired option by entering its number:")
+        print("""
+        1. run the bruteforce algorithm
+        2. run optimized algorithm
+        3. Quit the secondary menu, and return main menu\n
+        """)
+  
     def error_choice_option(self):
         """Prints an error message when the user's input is not understood."""
         print("\nI did not understand your request, try again\n\n")
