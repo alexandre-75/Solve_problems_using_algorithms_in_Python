2fbc1dc35c8e32788a597b0f5a45d66081ea3c41
sassy alexandre <sassy.alexandre@gmail.com>
2023-03-16 11:48:28 +0100

    Optimization Algorithm: The Backpack

--- controllers/optimizedcontroller.py
+++ controllers/optimizedcontroller.py
@@ -1,5 +1,6 @@
 from utils.constant import Constant
 from models.datamodel import DataModel
+from controllers.stockcontroller import Stock
 
 class OptimizedController():
 
@@ -13,15 +14,29 @@ class OptimizedController():
         action_instances_list = self.data_model.creating_a_list_of_stock_market_instances(file_path)
         total_action = len(action_instances_list)
 
-        matrix = [[0 for column  in range(3)] for row in range(total_action + 1)]
-        # print(matrix)
+        matrix = [[0 for column  in range(capital_to_invest + 1)] for row in range(total_action + 1)]
 
         share_price = []
         profitability_of_an_share = []
+        list_profit = []
         for share in action_instances_list:
             share_price.append(share.share_price)
             profitability_of_an_share.append(share.profitability_of_an_share)
-        # print(share_price)
-        # print(profitability_of_an_share)
+            list_profit.append((share.share_price/100)*share.profitability_of_an_share)
+      
+        for nb_of_share in range(1, total_action + 1):
+            current_share = action_instances_list[nb_of_share-1]
+            for evolving_capital in range (1, capital_to_invest + 1):
+                if current_share.share_price <= evolving_capital:
+                    matrix[nb_of_share][evolving_capital] = max(matrix[nb_of_share-1][evolving_capital], list_profit[nb_of_share-1] + matrix[nb_of_share-1][evolving_capital-share_price[nb_of_share-1]])
+                else:
+                    matrix[nb_of_share][evolving_capital] = matrix[nb_of_share-1][evolving_capital]
 
-
+        stock_selection = []
+        while capital_to_invest >= 0 and total_action >=0:
+            stock = action_instances_list[total_action - 1]
+            if matrix[int(total_action)][int(capital_to_invest)] == matrix[int(total_action)-1][int(capital_to_invest - stock.share_price)] + stock.profit2:
+                stock_selection.append(stock)
+                capital_to_invest -= stock.share_price
+            total_action -= 1
+        return matrix[-1][-1], stock_selection
--- controllers/stockcontroller.py
+++ controllers/stockcontroller.py
@@ -4,5 +4,6 @@ class Stock:
         self.name_of_an_action = name_of_an_action
         self.share_price = share_price
         self.profitability_of_an_share = profitability_of_an_share
+        self.profit2 = (self.share_price/100)*self.profitability_of_an_share
 
     
