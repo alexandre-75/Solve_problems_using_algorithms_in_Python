6b3885d1412588a40ed8a6c9099f133dcb35a084
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-04-16 13:44:59 +0200

    requirements + display view

--- views/optimizedview.py
+++ views/optimizedview.py
@@ -8,16 +8,16 @@ class OptimizedView():
 
         """Displays a summary of the investment, shares purchased, expected profit and execution time."""
         
-        if file_path == "data\dataset1_Python+P7.csv" or "data\dataset1_Python+P7.csv":
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            print("\nResume best investment :")
-            print(f"\nexpected profits : {profit/10000}€\n")
-            total_invested = []
-            for i in actions_list:
-                print(f"name : {i.name_of_an_action}, price : {i.share_price/100}€")
-                total_invested.append(i.share_price/100)
-            print(f"\ntotal invested : {sum(total_invested)}€")
-            print(f"\nprogram execution time (seconde) : {time}")
+        # if file_path == "data\dataset1_Python+P7.csv" or "data\dataset1_Python+P7.csv":
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("\nResume best investment :")
+        print(f"\nexpected profits : {profit/10000}€\n")
+        total_invested = []
+        for i in actions_list:
+            print(f"name : {i.name_of_an_action}, price : {i.share_price/100}€")
+            total_invested.append(i.share_price/100)
+        print(f"\ntotal invested : {sum(total_invested)}€")
+        print(f"\nprogram execution time (seconde) : {time}")
 
     def display_resume_investment_file_1(self, profit, actions_list, time):
 
